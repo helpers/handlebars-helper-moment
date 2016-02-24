f8469ea5b9169edae4908a03d283a28b5066b13d
ipaterson <ipaterson@newstex.com>
2016-02-24 12:56:14 -0500

    Ensures that formatting happens last if the handlebars hash does not iterate in the order written.
    Fixes a problem where formatting could happen before other operations, causing those operations to be ignored.

--- moment.js
+++ moment.js
@@ -21,16 +21,24 @@ module.exports.register = function (Handlebars, options, params) {
       context = undefined;
     }
     var date = moment(context);
+    var hasFormat = false;
 
     // Reset the language back to default before doing anything else
     date.lang('en');
 
     for (var i in block.hash) {
-      if (date[i]) {
+      if (i === 'format') {
+        hasFormat = true;
+      }
+      else if (date[i]) {
         date = date[i](block.hash[i]);
       } else {
         console.log('moment.js does not support "' + i + '"');
       }
+    }
+
+    if (hasFormat) {
+      date = date.format(block.hash.format);
     }
     return date;
   });
@@ -41,16 +49,24 @@ module.exports.register = function (Handlebars, options, params) {
       context = 0;
     }
     var duration = moment.duration(context);
+    var hasFormat = false;
 
     // Reset the language back to default before doing anything else
     duration = duration.lang('en');
 
     for (var i in block.hash) {
-      if (duration[i]) {
+      if (i === 'format') {
+        hasFormat = true;
+      }
+      else if (duration[i]) {
         duration = duration[i](block.hash[i]);
       } else {
         console.log('moment.js duration does not support "' + i + '"');
       }
+    }
+
+    if (hasFormat) {
+      duration = duration.format(block.hash.format);
     }
     return duration;
   });
